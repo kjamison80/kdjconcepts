f33d51638b68764487bd4702b1ed3325e906d70b
Kirstina Jamison <kirstinajamison@gmail.com>
2018-03-12 22:49:03 -0600

    next trial

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
+const app = next({ dir: '.', dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  const server = express()
+
+  server.get('/user/:id', (req, res) => {
+    const mergedQuery = Object.assign({}, req.query, req.params);
+    return app.render(req, res, '/user', mergedQuery)
+  })
+
+  server.get('/preact', (req, res) => {
+    return app.render(req, res, '/preact')
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
