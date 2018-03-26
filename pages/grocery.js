b53ff8d502ef04924c0e5536b89b2ff1af762104
Kirstina Jamison <kirstinajamison@gmail.com>
2018-03-25 23:07:12 -0600

    Tied up the start day dropdown.

--- pages/grocery.js
+++ pages/grocery.js
@@ -4,14 +4,31 @@ import withRedux from 'next-redux-wrapper';
 import { bindActionCreators } from 'redux';
 import Link from 'next/link'
 import Page from '../layouts/default';
-import { initStore, setDayQty, updateDaysToPlan } from '../stores/grocery';
+import { initStore, setDayQty, updateDaysToPlan, updateStartDay } from '../stores/grocery';
 import DayOptions from '../components/DayOptions';
 import MealSelector from './MealSelector';
 
 export class MyPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.setDayQty = this.setDayQty.bind(this);
+        this.updateStartDay = this.updateStartDay.bind(this);
+    }
+    
     setDayQty(e) {
-        this.props.setDayQty(e.currentTarget.value);
-        this.props.updateDaysToPlan(this.props.daysToPlan, e.currentTarget.value);
+        const { daysToPlan, setDayQty, updateDaysToPlan } = this.props;
+        const qty = parseInt(e.currentTarget.value, 10);
+
+        // TODO: there is still a bug where if day is set to not default 'sunday' and you backspace in the qty input to blank, then day resets but dropdown does not.
+        setDayQty(qty);
+        updateDaysToPlan(this.props.daysToPlan, qty);
+    }
+
+    updateStartDay(e) {
+        const { updateStartDay, daysToPlan, setDayQty } = this.props;
+
+        updateStartDay(daysToPlan, e.currentTarget.value);
     }
 
     render() {
@@ -27,13 +44,13 @@ export class MyPage extends Component {
                     <div className="col-wrap dw-50 tw-100">
                         <div className="col">
                             <label htmlFor="shoppingDay" className="block">Planned Shopping Day</label>
-                            <select id="shppingDay" className="caps">
+                            <select id="shppingDay" className="caps" onChange={this.updateStartDay}>
                                 {daysOfWeek.map(day => <option key={day.toLowerCase()} value={day.toLowerCase()}>{day}</option>)}
                             </select>
                         </div>
                         <div className="col dw-50">
                             <label htmlFor="qtyDays" className="block">Shopping for how many days?</label>
-                            <input className="ti qty" type="number" defaultValue={dayQty} onChange={e => this.setDayQty(e)} />
+                            <input className="ti qty" type="number" defaultValue={dayQty} onChange={this.setDayQty} />
                         </div>
                     </div>
                     <hr style={{margin: '1em .5em'}} />
@@ -51,6 +68,7 @@ const mapStateToProps = ({ dayQty, daysOfWeek, daysToPlan, mealSelectorOptions }
 
 const mapDispatchToProps = dispatch => ({
     updateDaysToPlan: bindActionCreators(updateDaysToPlan, dispatch),
+    updateStartDay: bindActionCreators(updateStartDay, dispatch),
     setDayQty: bindActionCreators(setDayQty, dispatch),
 });
 
--- stores/grocery.js
+++ stores/grocery.js
@@ -19,6 +19,7 @@ export const actionTypes = {
     SET_MEAL_SELECTOR_OPTIONS: 'set_meal_selector_options',
     SET_PLANNED_MEAL: 'set_planned_meal',
     UPDATE_DAYS_TO_PLAN: 'update_days_to_plan',
+    UPDATE_START_DAY: 'update_start_day',
 };
 
 // REDUCERS
@@ -31,6 +32,8 @@ export const reducer = (state = exampleInitialState, actions) => {
         case actionTypes.SET_PLANNED_MEAL:
             return { ...state, plannedMeals: actions.payload };
         case actionTypes.UPDATE_DAYS_TO_PLAN:
+            return { ...state, daysToPlan: actions.payload };
+        case actionTypes.UPDATE_START_DAY:
             return { ...state, daysToPlan: actions.payload };
         default: return state;
     }
@@ -45,17 +48,47 @@ export const reducer = (state = exampleInitialState, actions) => {
  */
 export const setDayQty = qty => dispatch => dispatch({ type: actionTypes.SET_DAY_QTY, payload: qty });
 
-
-export const updateDaysToPlan = (days, qty) => dispatch => {
-    const initialDays = [...exampleInitialState.daysToPlan];
-    const indexStart = days.length ? initialDays.indexOf(days[0]) : 0;
+/**
+ * Reusable function that creates an array of days to plan based on start day and qty of days to plan.
+ * @param currDays
+ * @param startDay
+ * @param qty
+ * @returns Array []
+ */
+const updateDays2 = (currDays, startDay, qty) => {
+    const daysOfWeek = [...exampleInitialState.daysOfWeek];
+    const indexStart = currDays.length ? daysOfWeek.indexOf(startDay) : 0;
     let updatedDays = [];
 
-    for (let i = indexStart; i < qty; i++) {
-        updatedDays = [...updatedDays, exampleInitialState.daysOfWeek[i < 7 ? i : i % 7]];
+    for (let i = indexStart; i < (qty + indexStart); i++) {
+        updatedDays = [...updatedDays, daysOfWeek[i % 7]];
     }
 
+    return [...updatedDays];
+}
+
+/**
+ * Updates the days to plan based on qty changes
+ * @param days
+ * @param qty
+ * @returns {function(dispatch)}
+ */
+export const updateDaysToPlan = (days, qty) => dispatch => {
+    const updatedDays = updateDays2(days, days[0], qty);
+
     return dispatch({ type: actionTypes.UPDATE_DAYS_TO_PLAN, payload: updatedDays });
+}
+
+/**
+ * Updates the days to plan based on start date changes.
+ * @param daysToPlan
+ * @param newStartDay
+ * @returns {function(dispatch)}
+ */
+export const updateStartDay = (daysToPlan, newStartDay) => dispatch => {
+    const updatedDays = updateDays2(daysToPlan, newStartDay, daysToPlan.length);
+
+    return dispatch({ type: actionTypes.UPDATE_START_DAY, payload: updatedDays});
 }
 
 /**
